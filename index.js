7bb9ec3bdb74701030e2e8b9384ee55ab8bd0afa
Balasubramani S <sbalasubramani23784@gmail.com>
2026-08-17 07:57:07 +0530

    static middleware added

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
